61714b1500751ab6f514c7b4ac37e2f9ec5e746d
brentnall <a.brentnall@qmul.ac.uk>
2026-08-04 17:11:36 +0100

    add subgroup analysis trials with data before mortality

--- 2_other/code/Allcancer_functions.R
+++ 2_other/code/Allcancer_functions.R
@@ -112,8 +112,9 @@ replace_with_reported <- function(data){
 
 # Create final table for analysis with mortality and surrogate estimates
 final_table <- function(mortality_data, surrogate_data){
-  mortality_data <- mortality_data %>%
-    select(trial_acronym, screening_test, outcome_description, numerator_screening, numerator_comparator, denominator_screening, denominator_comparator, yi, vi, sei, ci.lb, ci.ub)
+    mortality_data <- mortality_data %>%
+        mutate(yim2=as.numeric(yim2)) %>%
+    select(trial_acronym, screening_test, outcome_description, numerator_screening, numerator_comparator, denominator_screening, denominator_comparator, yi, vi, sei, ci.lb, ci.ub, yim2)
   
   data <- full_join(mortality_data, surrogate_data, by=c("trial_acronym", "screening_test"))
   
@@ -142,6 +143,49 @@ analysis <- function(finaldata_primary, cancer, surrogate, analysis, bootfunctio
   
   # correlation
   correlation <- weights::wtd.cor(finaldata_primary$yi_m, finaldata_primary$yi_s, weight=finaldata_primary$weight_var)
+  
+  results_surrogate <- data.frame(Cancer=cancer,
+                                  Surrogate=surrogate,
+                                  Analysis=analysis,
+                                  Ntrials=finaldata_primary$N_trials[1],
+                                  Intercept = as.numeric(m_reg_sum$coefficients[1]),
+                                  Intercept_lb_ci = cis[1,1], 
+                                  Intercept_ub_ci = cis[2,1],
+                                  Slope_lb_ci = cis[1,2],
+                                  Slope_ub_ci = cis[2,2],
+                                  Slope = as.numeric(m_reg_sum$coefficients[2]),
+                                  Correlation=correlation[1],
+                                  Correlation_lb_ci = cis[1,3],
+                                  Correlation_ub_ci = cis[2,3],
+                                  Rsquared=m_reg_sum$r.squared,
+                                  Rsquared_lb_ci = cis[1,4],
+                                  Rsquared_ub_ci = cis[2,4]
+    )
+  
+  #round results to 2 decimal places
+  results_surrogate <- results_surrogate %>%
+    mutate(across(where(is.numeric), ~ round(., 2)))
+}
+  
+
+# Analysis ----------------------------------------------------------------
+# - regression (through origin), weights: inverse variance
+# - weighted correlation
+# - returns line for results table
+## as above but uses rate difference for mortality, not relative risk
+analysis2 <- function(finaldata_primary, cancer, surrogate, analysis, bootfunction){
+
+  finaldata_primary <- finaldata_primary %>%
+    filter(!is.na(yi_s))
+    
+  # Regression (inverse variance weights)
+  m_reg <- lm(formula = yim2 ~ yi_s, data = finaldata_primary, weights = weight_var)
+  m_reg_sum <- summary(m_reg)
+  
+  cis <- bootfunction(finaldata_primary)
+  
+  # correlation
+  correlation <- weights::wtd.cor(finaldata_primary$yim2, finaldata_primary$yi_s, weight=finaldata_primary$weight_var)
   
   results_surrogate <- data.frame(Cancer=cancer,
                                   Surrogate=surrogate,
